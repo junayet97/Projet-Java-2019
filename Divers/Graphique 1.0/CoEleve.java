bf837fc5238362731c9e754099223586f5030fe7
GiraudonMathilde <50108534+GiraudonMathilde@users.noreply.github.com>
2019-06-06 19:40:26 +0200

    Add files via upload

---
+++ Divers/Graphique 1.0/CoEleve.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graphique;
+
+/**
+ *
+ * @author mathi
+ */
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+ 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+ 
+/**
+ * This program demonstrates how to use JPanel in Swing.
+ * @author www.codejava.net
+ */
+public class CoEleve extends JFrame implements ActionListener
+{
+    private JPanel jPanel1;
+    private JLabel id;
+    private JTextField caseid;
+    private JButton buttonvalider;
+    private JButton buttonretour;
+     
+    public CoEleve() 
+    {
+        super("Recherches Informations");
+        
+        //Initialisation
+        this.jPanel1 = new JPanel();
+        
+        JLabel logoece = new JLabel();
+        JLabel fond = new JLabel();
+        JLabel ptitre = new JLabel();
+        JLabel id = new JLabel();
+        JLabel gtitre = new JLabel();
+        
+        this.caseid = new JTextField();
+        
+        this.buttonvalider = new JButton();
+        this.buttonretour = new JButton();
+        
+        
+        //Parametre de la page
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);//termine le processus lorsque l'on clique sur la croix
+        setMinimumSize(new java.awt.Dimension(700, 500));
+        setPreferredSize(new java.awt.Dimension(700, 500));
+        setResizable(false);
+        setLocationRelativeTo(null); //fenêtre positionnée au centre de l'écran
+        setVisible(true); 
+        
+        jPanel1.setLayout(null);
+        
+        //Le logo
+        logoece.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Graphique/logoECE.jpg"))); // NOI18N
+        logoece.setText("jLabel1");
+        jPanel1.add(logoece);
+        logoece.setBounds(19, 11, 266, 67);
+        
+        //Le petit titre
+        ptitre.setFont(new java.awt.Font("Segoe Print", 1, 24)); // NOI18N
+        ptitre.setForeground(new java.awt.Color(204, 204, 255));
+        ptitre.setText("Indentifiez-vous :");
+        jPanel1.add(ptitre);
+        ptitre.setBounds(230, 150, 210, 44);
+        
+        //Le grand titre
+        gtitre.setFont(new java.awt.Font("Segoe Print", 1, 24)); // NOI18N
+        gtitre.setForeground(new java.awt.Color(204, 204, 255));
+        gtitre.setText("Recherche d'un Elève :");
+        jPanel1.add(gtitre);
+        gtitre.setBounds(330, 30, 330, 50);
+        
+        // l' ID
+        id.setFont(new java.awt.Font("Segoe Print", 0, 12)); // NOI18N
+        id.setForeground(new java.awt.Color(255, 255, 255));
+        id.setText("Rentrer un identifiant :");
+        jPanel1.add(id);
+        id.setBounds(90, 250, 143, 23);
+        
+        //La case pour rentrer id
+        jPanel1.add(caseid);
+        caseid.setBounds(270, 250, 200, 20);
+        
+        //Le bouton de la validation
+        buttonvalider.setFont(new java.awt.Font("Segoe Print", 0, 12)); // NOI18N
+        buttonvalider.setText("Valider");
+        jPanel1.add(buttonvalider);
+        buttonvalider.setBounds(495, 240, 100, 31);
+        
+        //Le bouton de retour
+        buttonretour.setFont(new java.awt.Font("Segoe Print", 0, 12)); // NOI18N
+        buttonretour.setText("Retour");
+        jPanel1.add(buttonretour);
+        buttonretour.setBounds(300, 360, 100, 31);
+        
+        //Le fond
+        fond.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Graphique/imagefond.jpg"))); // NOI18N
+        fond.setText("Recher");
+        fond.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+        jPanel1.add(fond);
+        fond.setBounds(0, 0, 710, 500);
+         
+        // faire du bouton une action
+        buttonvalider.addActionListener(this);
+        buttonretour.addActionListener(this);
+        caseid.addActionListener(this);
+         
+        // set border for the panel
+        jPanel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Informations Professeurs"));
+         
+        // add the panel to this frame
+        add(jPanel1);
+         
+        pack();
+    }
+    
+    
+    @Override
+    public  void actionPerformed(ActionEvent event)
+    {
+        if(event.getSource()== buttonvalider )
+        {
+            this.dispose();
+            new Info_Eleve();
+        }
+        
+        if(event.getSource()==buttonretour)
+        {
+            this.dispose();
+            new Menu_Principal();
+        }
+    }
+     
+}
---
+++ Divers/Graphique 1.0/Recherche.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graphique;
+
+/**
+ *
+ * @author mathi
+ */ 
+import Modèle.Connexion;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+ 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Recherche extends JFrame implements ActionListener 
+{
+    private JPanel jPanel1;
+    private JLabel logoece;
+    private JLabel titre;
+    private JButton buttonprof;
+    private JButton buttoneleve;
+    private JButton buttonretour;
+    private JLabel fonds;
+    
+    
+    public Recherche()
+    {
+        
+        super("Recherche");
+        //Inititalisation
+        
+        this.jPanel1 = new JPanel();
+        
+        JLabel logoece  = new JLabel();
+        JLabel titre = new JLabel();
+        
+        this.buttonprof= new JButton();
+        this.buttoneleve = new JButton();
+        this.buttonretour = new JButton();
+        JLabel fonds = new JLabel();
+        
+        //Parametrage de la page
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);//termine le processus lorsque l'on clique sur la croix
+        setMinimumSize(new java.awt.Dimension(700, 500));
+        setPreferredSize(new java.awt.Dimension(700, 500));
+        setResizable(false);
+        setLocationRelativeTo(null); //fenêtre positionnée au centre de l'écran
+        setVisible(true); 
+        
+        jPanel1.setLayout(null);
+        
+        //Le logo
+        logoece.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Graphique/logoECE.jpg"))); // NOI18N
+        logoece.setText("jLabel1");
+        jPanel1.add(logoece);
+        logoece.setBounds(10, 10, 270, 60);
+        
+        //Le titre
+        titre.setFont(new java.awt.Font("Segoe Print", 1, 24)); // NOI18N
+        titre.setForeground(new java.awt.Color(204, 204, 255));
+        titre.setText("Recherche d'une personne :");
+        jPanel1.add(titre);
+        titre.setBounds(170, 140, 330, 30);
+        
+        //Bouton Prof
+        buttonprof.setFont(new java.awt.Font("Segoe Print", 0, 12)); // NOI18N
+        buttonprof.setText("Professeur");
+        jPanel1.add(buttonprof);
+        buttonprof.setBounds(180, 230, 120, 50);
+        
+        //Bouton Elève
+        buttoneleve.setFont(new java.awt.Font("Segoe Print", 0, 12)); // NOI18N
+        buttoneleve.setText("Elève");
+        jPanel1.add(buttoneleve);
+        buttoneleve.setBounds(370, 230, 120, 50);
+        
+        //Bouton de Retour
+        buttonretour.setFont(new java.awt.Font("Segoe Print", 0, 12)); // NOI18N
+        buttonretour.setText("Retour");
+        jPanel1.add(buttonretour);
+        buttonretour.setBounds(290, 380, 80, 30);
+        
+        
+        //Le fond
+        fonds.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Graphique/imagefond.jpg"))); // NOI18N
+        fonds.setText("jLabel3");
+        jPanel1.add(fonds);
+        fonds.setBounds(4, 4, 700, 490);
+        
+        
+        // faire du bouton une action
+        this.buttoneleve.addActionListener(this);
+        this.buttonprof.addActionListener(this);
+        this.buttonretour.addActionListener(this);
+        
+        
+        // set border for the panel
+        jPanel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Page de Recherche"));
+      
+        
+        // add the panel to this frame
+        add(jPanel1);
+ 
+        pack();
+      
+    }
+   
+    @Override
+    public  void actionPerformed(ActionEvent event)
+    {
+        if(event.getSource()== buttonprof)
+        {
+            this.dispose();
+            new CoProf();
+        }                
+        if(event.getSource()== buttoneleve)
+        {
+            this.dispose();
+            new CoEleve();
+        }
+        if(event.getSource()== buttonretour)
+        {
+            this.dispose();
+            new Menu_Principal();
+        }
+    }
+}
+
+
+
+
